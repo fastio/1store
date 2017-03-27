398245fcba51268ed992efc478018581cfe42993
fake <fake>
2017-03-27 20:14:50 +0800

    change the license to AGPL

--- redis_timer_set.hh
+++ redis_timer_set.hh
@@ -10,6 +10,11 @@
  * This work is open source software, licensed under the terms of the
  * BSD license as described in the LICENSE file in the top-level directory.
  */
+
+/*
+ * Modified by Peng Jian, pstack@163.com.
+ */ 
+
 #pragma once
 #include <chrono>
 #include <limits>
