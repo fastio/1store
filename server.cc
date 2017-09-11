e0664eb11c069a962ee8b56a03c64ffefebcf588
Peng Jian <pstack@163.com>
2017-09-11 09:28:05 +0800

    impl server

--- server.cc
+++ server.cc
@@ -1,34 +1,188 @@
-/*
-* Pedis is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* You may obtain a copy of the License at
-*
-*     http://www.gnu.org/licenses
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-* 
-*  Copyright (c) 2016-2026, Peng Jian, pstack@163.com. All rights reserved.
-*
-*/
 #include "server.hh"
+
+#include <boost/bimap/unordered_set_of.hpp>
+#include <boost/range/irange.hpp>
+#include <boost/bimap.hpp>
+#include <boost/assign.hpp>
+#include <boost/locale/encoding_utf.hpp>
+#include <boost/range/adaptor/sliced.hpp>
+
+#include "service.hh"
+#include "core/future-util.hh"
+#include "core/reactor.hh"
+#include "net/byteorder.hh"
+#include <seastar/core/metrics.hh>
+#include <seastar/net/byteorder.hh>
+#include <seastar/util/lazy.hh>
+#include <seastar/core/execution_stage.hh>
+
+#include "exceptions/exceptions.hh"
+#include <cassert>
+#include <string>
+
+#include <snappy-c.h>
+#include <lz4.h>
+
 namespace redis {
+
+static logging::logger rlog("server");
 
 distributed<server> _the_server;
 
-void server::setup_metrics()
+server::server(distributed<redis::proxy>& p)
+    : _proxy(p)
+    , _max_request_size(memory::stats().total_memory() / 10)
+    , _memory_available(_max_request_size)
 {
     namespace sm = seastar::metrics;
-    _metrics.add_group("connections", {
-        sm::make_counter("opened_total", [this] { return _stats._connections_total; }, sm::description("Total number of connections opened.")),
-        sm::make_counter("current_total", [this] { return _stats._connections_current; }, sm::description("Total number of connections current opened.")),
+
+    _metrics.add_group("transport", {
+        sm::make_derive("cql-connections", _connects,
+                        sm::description("Counts a number of client connections.")),
+
+        sm::make_gauge("current_connections", _connections,
+                        sm::description("Holds a current number of client connections.")),
+
+        sm::make_derive("requests_served", _requests_served,
+                        sm::description("Counts a number of served requests.")),
+
+        sm::make_gauge("requests_serving", _requests_serving,
+                        sm::description("Holds a number of requests that are being processed right now.")),
+
+        sm::make_gauge("requests_blocked_memory", [this] { return _memory_available.waiters(); },
+                        sm::description(
+                            seastar::format("Holds a number of requests that are blocked due to reaching the memory quota limit ({}B). "
+                                            "Non-zero value indicates that our bottleneck is memory and more specifically - the memory quota allocated for the \" redis\" component.", _max_request_size))),
     });
+}
+
+future<> server::stop() {
+    _stopping = true;
+    size_t nr = 0;
+    size_t nr_total = _listeners.size();
+    rlog.debug("server: abort accept nr_total={}", nr_total);
+    for (auto&& l : _listeners) {
+        l.abort_accept();
+        rlog.debug("server: abort accept {} out of {} done", ++nr, nr_total);
+    }
+    auto nr_conn = make_lw_shared<size_t>(0);
+    auto nr_conn_total = _connections_list.size();
+    rlog.debug("server: shutdown connection nr_total={}", nr_conn_total);
+    return parallel_for_each(_connections_list.begin(), _connections_list.end(), [nr_conn, nr_conn_total] (auto&& c) {
+        return c.shutdown().then([nr_conn, nr_conn_total] {
+            rlog.debug("server: shutdown connection {} out of {} done", ++(*nr_conn), nr_conn_total);
+        });
+    }).then([this] {
+        return std::move(_stopped);
+    });
+}
+
+future<>
+server::listen(ipv4_addr addr, std::shared_ptr<seastar::tls::credentials_builder> creds, bool keepalive) {
+    listen_options lo;
+    lo.reuse_address = true;
+    server_socket ss;
+    try {
+        ss = creds
+          ? seastar::tls::listen(creds->build_server_credentials(), make_ipv4_address(addr), lo)
+          : engine().listen(make_ipv4_address(addr), lo);
+    } catch (...) {
+        throw std::runtime_error(sprint("redis server error while listening on %s -> %s", make_ipv4_address(addr), std::current_exception()));
+    }
+    _listeners.emplace_back(std::move(ss));
+    _stopped = when_all(std::move(_stopped), do_accepts(_listeners.size() - 1, keepalive, addr)).discard_result();
+    return make_ready_future<>();
+}
+
+future<>
+server::do_accepts(int which, bool keepalive, ipv4_addr server_addr) {
+    ++_connections_being_accepted;
+    return _listeners[which].accept().then_wrapped([this, which, keepalive, server_addr] (future<connected_socket, socket_address> f_cs_sa) mutable {
+        --_connections_being_accepted;
+        if (_stopping) {
+            f_cs_sa.ignore_ready_future();
+            maybe_idle();
+            return make_ready_future<>();
+        }
+        auto cs_sa = f_cs_sa.get();
+        auto fd = std::get<0>(std::move(cs_sa));
+        auto addr = std::get<1>(std::move(cs_sa));
+        fd.set_nodelay(true);
+        fd.set_keepalive(keepalive);
+        auto conn = make_shared<connection>(*this, server_addr, std::move(fd), std::move(addr));
+        ++_connects;
+        ++_connections;
+        conn->process().then_wrapped([this, conn] (future<> f) {
+            --_connections;
+            try {
+                f.get();
+            } catch (...) {
+                rlog.debug("connection error: {}", std::current_exception());
+            }
+        });
+        return do_accepts(which, keepalive, server_addr);
+    }).then_wrapped([this, which, keepalive, server_addr] (future<> f) {
+        try {
+            f.get();
+        } catch (...) {
+            rlog.warn("acccept failed: {}", std::current_exception());
+            return do_accepts(which, keepalive, server_addr);
+        }
+        return make_ready_future<>();
+    });
+}
+
+server::connection::connection(server& server, ipv4_addr server_addr, connected_socket&& fd, socket_address addr)
+    : _server(server)
+    , _server_addr(server_addr)
+    , _fd(std::move(fd))
+    , _in(_fd.input())
+    , _out(_fd.output())
+{
+    ++_server._total_connections;
+    ++_server._current_connections;
+    _server._connections_list.push_back(*this);
+}
+
+server::connection::~connection() {
+    --_server._current_connections;
+    _server._connections_list.erase(_server._connections_list.iterator_to(*this));
+    _server.maybe_idle();
+}
+
+future<> server::connection::process()
+{
+    return do_until([this] { return _in.eof(); }, [this] {
+        return with_gate(_pending_requests_gate, [this] {
+            return process_request();
+        });
+    }).then_wrapped([this] (future<> f) {
+        try {
+            f.get();
+            return make_ready_future<>();
+        } catch (...) {
+            throw request_exception("request error");
+        }
+    }).finally([this] {
+        return _pending_requests_gate.close().then([this] {
+            return _ready_to_respond.finally([this] {
+                return _out.close();
+            });
+        });
+    });
+}
+
+future<> server::connection::shutdown()
+{
+    try {
+        _fd.shutdown_input();
+        _fd.shutdown_output();
+    } catch (...) {
+    }
+    return make_ready_future<>();
+}
+
+future<> server::connection::process_request() {
+    return make_ready_future<>();
 }
 }
--- server.hh
+++ server.hh
@@ -1,31 +1,18 @@
-/*
-* Pedis is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* You may obtain a copy of the License at
-*
-*     http://www.gnu.org/licenses
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-* 
-*  Copyright (c) 2016-2026, Peng Jian, pstack@163.com. All rights reserved.
-*
-*/
 #pragma once
-#include "redis.hh"
+
+#include "proxy.hh"
 #include "db.hh"
-#include "redis.hh"
 #include "redis_protocol.hh"
-#include "core/metrics_registration.hh"
-#include "core/thread.hh"
+#include <memory>
+#include <boost/intrusive/list.hpp>
+#include "core/reactor.hh"
+#include "core/distributed.hh"
+#include "core/semaphore.hh"
 #include "core/gate.hh"
+#include "net/tls.hh"
+#include "core/metrics_registration.hh"
+#include "seastarx.hh"
+
 namespace redis {
 
 class server;
@@ -33,69 +20,56 @@ extern distributed<server> _the_server;
 inline distributed<server>& get_server() {
     return _the_server;
 }
+
 class server {
 private:
-    lw_shared_ptr<server_socket> _listener;
-    uint16_t _port;
-    struct connection {
-        connected_socket _socket;
-        socket_address _addr;
+    std::vector<server_socket> _listeners;
+    distributed<redis::proxy>& _proxy;
+    size_t _max_request_size;
+    semaphore _memory_available;
+    seastar::metrics::metric_groups _metrics;
+private:
+    uint64_t _connects = 0;
+    uint64_t _connections = 0;
+    uint64_t _requests_served = 0;
+    uint64_t _requests_serving = 0;
+public:
+    server(distributed<redis::proxy>& p);
+    future<> listen(ipv4_addr addr, std::shared_ptr<seastar::tls::credentials_builder> = {}, bool keepalive = false);
+    future<> do_accepts(int which, bool keepalive, ipv4_addr server_addr);
+    future<> stop();
+public:
+private:
+    class connection : public boost::intrusive::list_base_hook<> {
+        server& _server;
+        ipv4_addr _server_addr;
+        connected_socket _fd;
         input_stream<char> _in;
         output_stream<char> _out;
-        redis_protocol _proto;
-        connection(connected_socket&& socket, socket_address addr)
-            : _socket(std::move(socket))
-              , _addr(addr)
-              , _in(_socket.input())
-              , _out(_socket.output())
-        {
-        }
-        ~connection() {
-        }
+        seastar::gate _pending_requests_gate;
+        future<> _ready_to_respond = make_ready_future<>();
+        unsigned _request_cpu = 0;
+        redis_protocol _protocol;
+    public:
+        connection(server& server, ipv4_addr server_addr, connected_socket&& fd, socket_address addr);
+        ~connection();
+        future<> process();
+        future<> process_request();
+        future<> shutdown();
     };
-    seastar::metrics::metric_groups _metrics;
-    void setup_metrics();
-    struct stats {
-        uint64_t _connections_current = 0;
-        uint64_t _connections_total = 0;
-    };
-    stats _stats;
-    seastar::gate _request_gate;
-public:
-    server(uint16_t port = 6379)
-        : _port(port)
-    {
-        setup_metrics();
-    }
-
-    void start() {
-/*
-        listen_options lo;
-        lo.reuse_address = true;
-        _listener = engine().listen(make_ipv4_address({_port}), lo);
-        keep_doing([this] {
-           return _listener->accept().then([this] (connected_socket fd, socket_address addr) mutable {
-                   ++_stats._connections_total;
-                   ++_stats._connections_current;
-                   auto conn = make_lw_shared<connection>(std::move(fd), addr);
-                   do_until([conn] { return conn->_in.eof(); }, [this, conn] {
-                       return with_gate(_request_gate, [this, conn] {
-                           return conn->_proto.handle(conn->_in, conn->_out, _latency_tracer).then([this, conn] {
-                               return conn->_out.flush();
-                           });
-                       });
-                   }).finally([this, conn] {
-                       --_stats._connections_current;
-                       return conn->_out.close().finally([conn]{});
-                   });
-               });
-       }).or_terminate();
-*/
-    }
-    future<> stop() {
-        return _request_gate.close().then([this] {
-           return make_ready_future<>();
-        });
+private:
+    bool _stopping = false;
+    promise<> _all_connections_stopped;
+    future<> _stopped = _all_connections_stopped.get_future();
+    boost::intrusive::list<connection> _connections_list;
+    uint64_t _total_connections = 0;
+    uint64_t _current_connections = 0;
+    uint64_t _connections_being_accepted = 0;
+private:
+    void maybe_idle() {
+        if (_stopping && !_connections_being_accepted && !_current_connections) {
+            _all_connections_stopped.set_value();
+        }
     }
 };
-} /* namespace redis */
+}
