3f3cba1ca57e8f442d5f618e59ea87c3ad91d643
Peng Jian <pengjian.uestc@gmail.com>
2019-03-02 21:52:56 +0800

    add redis_mutation<T> represents the redis structure's mutation

---
+++ redis/redis_mutation.cc
@@ -0,0 +1,183 @@
+#include "redis_mutation.hh"
+#include "cql3/query_options.hh"
+#include "timeout_config.hh"
+#include "service/client_state.hh"
+#include "service/storage_proxy.hh"
+#include "dht/i_partitioner.hh"
+#include "partition_slice_builder.hh"
+#include "query-result-reader.hh"
+#include "gc_clock.hh"
+#include <boost/range/adaptor/transformed.hpp>
+#include <boost/range/algorithm_ext/push_back.hpp>
+#include <boost/range/adaptor/filtered.hpp>
+#include <boost/range/adaptor/map.hpp>
+#include <boost/range/adaptor/indirected.hpp>
+#include <boost/iterator/transform_iterator.hpp>
+#include <boost/range/adaptor/reversed.hpp>
+#include <memory>
+#include "seastar/core/sstring.hh"
+using namespace seastar;
+namespace redis {
+
+precision_time precision_time::get_next(db_clock::time_point millis) {
+    auto next =  millis < _last._millis
+        ? precision_time{millis, 9999}
+    : precision_time{millis,
+        std::max(0, _last._nanos - 1)};
+    _last = next;
+    return next;
+}
+constexpr const db_clock::time_point precision_time::REFERENCE_TIME;
+thread_local precision_time precision_time::_last = {db_clock::time_point::max(), 0};
+
+
+inline sstring make_sstring(bytes b) {
+    return sstring{reinterpret_cast<const char*>(b.data()), b.size()};
+}
+
+namespace internal {
+atomic_cell make_dead_cell() {
+    return atomic_cell::make_dead(api::new_timestamp(), gc_clock::now());
+}   
+
+atomic_cell make_cell(const schema_ptr schema,
+   const abstract_type& type,
+   const fragmented_temporary_buffer::view& value,
+   atomic_cell::collection_member cm = atomic_cell::collection_member::no)
+{
+   //auto ttl = _ttl;
+   //if (ttl.count() <= 0) {
+   auto ttl = schema->default_time_to_live();
+   //}   
+
+   if (ttl.count() > 0) {
+       return atomic_cell::make_live(type, api::new_timestamp(), value, gc_clock::now() + ttl, ttl, cm);
+   } else {
+       return atomic_cell::make_live(type, api::new_timestamp(), value, cm);
+   }   
+}  
+
+atomic_cell make_cell(const schema_ptr schema,
+    const abstract_type& type,
+    bytes_view value,
+    atomic_cell::collection_member cm = atomic_cell::collection_member::no)
+{
+    return make_cell(schema, type, fragmented_temporary_buffer::view(value), cm);
+}   
+
+mutation make_mutation(seastar::lw_shared_ptr<redis_mutation<bytes>> r)
+{
+    // redis table's partition key is always text type.
+    auto schema = r->schema();
+    const column_definition& column = *schema->get_column_definition("data");
+    auto pkey = partition_key::from_single_value(*schema, utf8_type->decompose(make_sstring(r->key())));
+    auto m = mutation(schema, std::move(pkey));
+    auto cell = make_cell(schema, *(column.type.get()), r->data()); 
+    m.set_clustered_cell(clustering_key::make_empty(), column, std::move(cell));
+    return std::move(m);
+}
+
+mutation make_mutation(seastar::lw_shared_ptr<redis_mutation<partition_dead_tag>> r)
+{
+    // redis table's partition key is always text type.
+    auto schema = r->schema();
+    auto pkey = partition_key::from_single_value(*schema, utf8_type->decompose(make_sstring(r->key())));
+    auto m = mutation(schema, std::move(pkey));
+    m.partition().apply(tombstone { api::new_timestamp(), gc_clock::now() });
+    return std::move(m);
+}
+
+mutation make_mutation(seastar::lw_shared_ptr<list_mutation> r)
+{
+    std::function<std::array<int8_t, 16>()> ug = nullptr;
+    if (r->data()._reversed) {
+        auto time = precision_time::REFERENCE_TIME - (db_clock::now() - precision_time::REFERENCE_TIME);
+        ug = [time = std::move(time)] () {
+            auto&& pt = precision_time::get_next(time);
+            auto uuid = utils::UUID_gen::get_time_UUID_bytes(pt._millis.time_since_epoch().count(), pt._nanos);
+            return uuid;
+        };
+    } else {
+        ug = [] () {
+            return utils::UUID_gen::get_time_UUID_bytes();
+        };
+    }
+    auto schema = r->schema();
+    const column_definition& column = *schema->get_column_definition("data");
+    auto pkey = partition_key::from_single_value(*schema, utf8_type->decompose(make_sstring(r->key())));
+    auto m = mutation(schema, std::move(pkey));
+    auto&& ltype = static_cast<const list_type_impl*>(column.type.get());
+    list_type_impl::mutation lm;
+    lm.cells.reserve(r->data().size());
+    for (auto&& value : r->data()._cells) {
+        auto uuid = ug();
+        lm.cells.emplace_back(bytes(uuid.data(), uuid.size()), make_cell(schema, *ltype->value_comparator(), std::move(value), atomic_cell::collection_member::yes));
+    }
+    if (r->data()._reversed) {
+        std::reverse(lm.cells.begin(), lm.cells.end());
+    }
+    m.set_cell(clustering_key::make_empty(), column, atomic_cell_or_collection::from_collection_mutation(ltype->serialize_mutation_form(std::move(lm))));
+    return std::move(m);
+}
+
+mutation make_mutation(seastar::lw_shared_ptr<list_indexed_cells_mutation> r)
+{
+    auto schema = r->schema();
+    const column_definition& column = *schema->get_column_definition("data");
+    auto pkey = partition_key::from_single_value(*schema, utf8_type->decompose(make_sstring(r->key())));
+    auto m = mutation(schema, std::move(pkey));
+    auto&& ltype = static_cast<const list_type_impl*>(column.type.get());
+    list_type_impl::mutation lm;
+    lm.cells.reserve(r->data().size());
+    for (auto&& data : r->data()._indexed_cells) {
+        lm.cells.emplace_back(data.first, make_cell(schema, *ltype->value_comparator(), std::move(data.second), atomic_cell::collection_member::yes));
+    }
+    m.set_cell(clustering_key::make_empty(), column, atomic_cell_or_collection::from_collection_mutation(ltype->serialize_mutation_form(std::move(lm))));
+    return std::move(m);
+}
+
+mutation make_mutation(seastar::lw_shared_ptr<list_dead_cells_mutation> r)
+{
+    auto schema = r->schema();
+    const column_definition& column = *schema->get_column_definition("data");
+    auto pkey = partition_key::from_single_value(*schema, utf8_type->decompose(make_sstring(r->key())));
+    auto m = mutation(schema, std::move(pkey));
+    auto&& ltype = static_cast<const list_type_impl*>(column.type.get());
+    list_type_impl::mutation lm;
+    
+    lm.cells.reserve(r->data().size());
+    for (auto&& ckey : r->data()._cell_keys) {
+        lm.cells.emplace_back(ckey, make_dead_cell());
+    }    
+    m.set_cell(clustering_key::make_empty(), column, atomic_cell_or_collection::from_collection_mutation(ltype->serialize_mutation_form(std::move(lm))));
+    return std::move(m);
+}
+
+future<> write_mutation_impl(service::storage_proxy& proxy,
+    std::vector<mutation>&& ms,
+    db::consistency_level cl,
+    db::timeout_clock::time_point timeout,
+    service::client_state& cs) 
+{
+    return proxy.mutate_atomically(std::move(ms), cl, timeout, nullptr);
+}
+
+}
+
+future<> write_mutations(
+    service::storage_proxy& proxy,
+    std::vector<seastar::lw_shared_ptr<redis_mutation<bytes>>> ms,
+    db::consistency_level cl,
+    db::timeout_clock::time_point timeout,
+    service::client_state& client_state
+)
+{
+    return do_with(std::move(ms), [&proxy, cl, timeout, &client_state] (auto& ms) {
+        return parallel_for_each(ms.begin(), ms.end(), [&proxy, timeout, cl, &client_state] (auto& r) {
+            auto m = internal::make_mutation(r);
+            return internal::write_mutation_impl(proxy, std::vector<mutation> { std::move(m) }, cl ,timeout, client_state).finally([r] {});
+        });
+    });
+}
+
+} // end of redis namespace
---
+++ redis/redis_mutation.hh
@@ -0,0 +1,143 @@
+#pragma once
+#include "bytes.hh"
+#include "seastar/core/future.hh"
+#include "seastar/core/sstring.hh"
+#include "redis/request.hh"
+#include "redis/reply.hh"
+#include "mutation.hh"
+#include "db/consistency_level_type.hh"
+#include "db/timeout_clock.hh"
+#include "db/system_keyspace.hh"
+#include "service/storage_proxy.hh"
+#include "keys.hh"
+#include "timestamp.hh"
+#include "redis/redis_keyspace.hh"
+#include <unordered_map>
+
+using namespace seastar;
+
+class timeout_config;
+
+namespace service {
+//class storage_proxy;
+class client_state;
+}
+
+namespace cql3 {
+class query_options;
+}
+
+namespace tracing {
+class trace_state_ptr;
+}
+
+namespace redis {
+
+class precision_time {
+   public:
+       static constexpr db_clock::time_point REFERENCE_TIME{std::chrono::milliseconds(1262304000000)};
+   private:
+       static thread_local precision_time _last;
+   public:
+       db_clock::time_point _millis;
+       int32_t _nanos;
+
+       static precision_time get_next(db_clock::time_point millis);
+};  
+
+using partition_dead_tag = char;  
+struct cell_dead_tag {};
+
+template<typename ContainerType>
+struct redis_mutation {
+    const schema_ptr _schema;
+    bytes _partition_key;
+    ContainerType _data;
+    redis_mutation(const schema_ptr schema, const bytes& key, ContainerType&& data)
+        : _schema(schema)
+        , _partition_key(key)
+        , _data(std::move(data))
+    {
+    }
+    ~redis_mutation()
+    {
+    }
+    const schema_ptr schema() const { return _schema; }
+    ContainerType& data() { return _data; }
+    bytes& key() { return _partition_key; }
+};
+
+struct list_cells {
+    std::vector<bytes> _cells;
+    bool _reversed;
+    size_t size() const { return _cells.size(); }
+    list_cells(std::vector<bytes>&& cells, bool reversed) : _cells(std::move(cells)), _reversed(reversed) {}
+};
+using list_mutation = redis_mutation<list_cells>;
+
+struct list_indexed_cells {
+    std::vector<std::pair<bytes, bytes>> _indexed_cells;
+    size_t size() const { return _indexed_cells.size(); }
+    list_indexed_cells(std::vector<std::pair<bytes, bytes>>&& indexed_cells) : _indexed_cells(std::move(indexed_cells)) {}
+};
+using list_indexed_cells_mutation = redis_mutation<list_indexed_cells>;
+
+struct list_dead_cells {
+    std::vector<bytes> _cell_keys;
+    size_t size() const { return _cell_keys.size(); }
+    list_dead_cells(std::vector<bytes>&& cell_keys) : _cell_keys(std::move(cell_keys)) {}
+};
+using list_dead_cells_mutation = redis_mutation<list_dead_cells>;
+
+static inline seastar::lw_shared_ptr<redis_mutation<bytes>> make_simple(const schema_ptr schema, const bytes& key, bytes&& data) {
+    return seastar::make_lw_shared<redis_mutation<bytes>>(schema, key, std::move(data));
+}
+static inline seastar::lw_shared_ptr<redis_mutation<partition_dead_tag>> make_dead(const schema_ptr schema, const bytes& key) {
+    return seastar::make_lw_shared<redis_mutation<partition_dead_tag>>(schema, key, std::move(partition_dead_tag { 0 }));
+}
+static inline seastar::lw_shared_ptr<list_mutation> make_list_cells(const schema_ptr schema, const bytes& key, std::vector<bytes>&& cells, bool reversed) {
+    return seastar::make_lw_shared<list_mutation> (schema, key, std::move(list_cells (std::move(cells), reversed)));
+}
+static inline seastar::lw_shared_ptr<list_indexed_cells_mutation> make_list_indexed_cells(const schema_ptr schema, const bytes& key, std::vector<std::pair<bytes, bytes>>&& indexed_cells) {
+    return seastar::make_lw_shared<list_indexed_cells_mutation> (schema, key, std::move(list_indexed_cells (std::move(indexed_cells))));
+}
+static inline seastar::lw_shared_ptr<list_dead_cells_mutation> make_list_dead_cells(const schema_ptr schema, const bytes& key, std::vector<bytes>&& cell_keys) {
+    return seastar::make_lw_shared<list_dead_cells_mutation> (schema, key, std::move(list_dead_cells (std::move(cell_keys))));
+}
+
+namespace internal {
+mutation make_mutation(seastar::lw_shared_ptr<redis_mutation<bytes>> r);
+mutation make_mutation(seastar::lw_shared_ptr<redis_mutation<partition_dead_tag>> r);
+mutation make_mutation(seastar::lw_shared_ptr<list_mutation> r);
+mutation make_mutation(seastar::lw_shared_ptr<list_indexed_cells_mutation> r);
+mutation make_mutation(seastar::lw_shared_ptr<list_dead_cells_mutation> r);
+future<> write_mutation_impl(
+    service::storage_proxy&,
+    std::vector<mutation>&& ms,
+    db::consistency_level cl,
+    db::timeout_clock::time_point timeout,
+    service::client_state& client_state
+);
+}
+
+template<typename ContainerType>
+future<> write_mutation(
+    service::storage_proxy& proxy,
+    seastar::lw_shared_ptr<redis_mutation<ContainerType>> r,
+    db::consistency_level cl,
+    db::timeout_clock::time_point timeout,
+    service::client_state& client_state
+)
+{
+    return internal::write_mutation_impl(proxy, std::vector<mutation> { std::move(internal::make_mutation(r)) }, cl ,timeout, client_state).finally([r] {});
+}
+
+future<> write_mutations(
+    service::storage_proxy& proxy,
+    std::vector<seastar::lw_shared_ptr<redis_mutation<bytes>>> ms,
+    db::consistency_level cl,
+    db::timeout_clock::time_point timeout,
+    service::client_state& client_state
+);
+
+} // end of redis namespace
