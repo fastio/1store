4b8b34c33a3ed86ea0f907dccdcc52a6fcc6109d
Peng Jian <pengjian.uestc@gmail.com>
2019-03-23 23:36:34 +0800

    Add test case for redis protocol

--- tests/redis/kv_test.cc
+++ tests/redis/kv_test.cc
@@ -6,11 +6,23 @@
 #include "utils/hash.hh"
 
 #include "tests/cql_test_env.hh"
+#include "tests/cql_assertions.hh"
+#include "redis/redis_keyspace.hh"
 
 SEASTAR_TEST_CASE(test_redis_set){
     return do_with_redis_env_thread([] (auto& e) {
-        return e.execute_redis_command("set a b").then([] (auto) {
+        e.execute_redis_command("set a b").then([] (auto) {
             return make_ready_future<>();
-        });
+        }).get0();
+
+        auto msg = e.execute_cql(sprint("select * from redis.%s ", redis::SIMPLE_OBJECTS)).get0();
+        assert_that(msg).is_rows()
+            .with_size(1)
+            .with_row({
+                      {bytes_type->decompose(data_value(bytes("a")))},
+                      {bytes_type->decompose(data_value(bytes("b")))},
+                      });
+        return make_ready_future<>();
     });
 }
+
