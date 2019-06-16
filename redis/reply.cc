e296305dfc9f17ce18984d569b46f286632ee629
Peng Jian <pengjian.uestc@gmail.com>
2019-06-16 19:57:08 +0800

    refine the redis message

---
+++ redis/reply.cc
@@ -0,0 +1,104 @@
+#include "reply.hh"
+#include "log.hh"
+namespace redis {
+static logging::logger logging("reply");
+future<redis_message> redis_message::make_list_bytes(map_return_type r, size_t begin, size_t end) {
+    auto m = make_lw_shared<scattered_message<char>> ();
+    m->append(sstring(sprint("*%d\r\n", end - begin + 1)));
+    auto& data = r->data();
+    for (size_t i = 0; i < data.size(); ++i) {
+        if (i >= begin && i <= end) {
+            write_bytes(m, *(data[i].first));
+        }
+    }
+    m->on_delete([ r = std::move(foreign_ptr { r }) ] {});
+    return make_ready_future<redis_message>(m);
+}
+
+future<redis_message> redis_message::make_list_bytes(map_return_type r, size_t index) {
+    assert(r->has_data());
+    assert(r->data().size() > 0);
+    assert(index >= 0 && index < r->data().size());
+
+    auto m = make_lw_shared<scattered_message<char>> ();
+    auto& data = *(r->data()[index].second);
+
+    write_bytes(m, data);
+    m->on_delete([ r = std::move(foreign_ptr { r }) ] {});
+
+    return make_ready_future<redis_message>(m);
+}
+
+future<redis_message> redis_message::make_map_key_bytes(map_return_type r) {
+    auto m = make_lw_shared<scattered_message<char>> ();
+    auto& data = r->data();
+    m->append(sstring(sprint("*%d\r\n", data.size())));
+    for (size_t i = 0; i < data.size(); ++i) {
+        write_bytes(m, *(data[i].first));
+    }
+    m->on_delete([ r = std::move(foreign_ptr { r }) ] {});
+    return make_ready_future<redis_message>(m);
+}
+
+future<redis_message> redis_message::make_map_val_bytes(map_return_type r) {
+    auto m = make_lw_shared<scattered_message<char>> ();
+    auto& data = r->data();
+    m->append(sstring(sprint("*%d\r\n", data.size())));
+    for (size_t i = 0; i < data.size(); ++i) {
+        write_bytes(m, *(data[i].second));
+    }
+    m->on_delete([ r = std::move(foreign_ptr { r }) ] {});
+    return make_ready_future<redis_message>(m);
+}
+
+future<redis_message> redis_message::make_map_bytes(map_return_type r) {
+    auto m = make_lw_shared<scattered_message<char>> ();
+    auto& data = r->data();
+    m->append(sstring(sprint("*%d\r\n", 2 * data.size())));
+    for (size_t i = 0; i < data.size(); ++i) {
+        write_bytes(m, *(data[i].first));
+        write_bytes(m, *(data[i].second));
+    }
+    m->on_delete([ r = std::move(foreign_ptr { r }) ] {});
+    return make_ready_future<redis_message>(m);
+}
+
+future<redis_message> redis_message::make_set_bytes(map_return_type r, size_t index) {
+    return make_set_bytes(r, std::vector<size_t> { index }); 
+}
+
+future<redis_message> redis_message::make_set_bytes(map_return_type r, std::vector<size_t> indexes) {
+    auto m = make_lw_shared<scattered_message<char>> ();
+    auto& data = r->data();
+
+    m->append(sstring(sprint("*%d\r\n", indexes.size())));
+    for (auto index : indexes) {
+        assert(index >= 0 && index < data.size());
+        write_bytes(m, *(data[index].first));
+    }
+    m->on_delete([ r = std::move(foreign_ptr { r }) ] {});
+    return make_ready_future<redis_message>(m);
+}
+
+future<redis_message> redis_message::make_zset_bytes(lw_shared_ptr<std::vector<std::optional<bytes>>> r) {
+    auto m = make_lw_shared<scattered_message<char>> ();
+    m->append(sstring(sprint("*%d\r\n", r->size())));
+    for (auto& e : *r) {
+        auto& b = *e;
+        logging.info("zset e = {}", sstring(reinterpret_cast<const char*>(b.data()), b.size()));
+        write_bytes(m, *e);
+    }
+    m->on_delete([ r = std::move(foreign_ptr { r }) ] {});
+    return make_ready_future<redis_message>(m);
+}
+future<redis_message> redis_message::make_mbytes(mbytes_return_type r) {
+    auto m = make_lw_shared<scattered_message<char>> ();
+    auto& data = r->data();
+    m->append(sstring(sprint("*%d\r\n", data.size())));
+    for (auto e : data) {
+        write_bytes(m, e.second);
+    }
+    m->on_delete([ r = std::move(foreign_ptr { r }) ] {});
+    return make_ready_future<redis_message>(m);
+}
+}
--- redis/reply.hh
+++ redis/reply.hh
@@ -30,161 +30,144 @@
 #include "seastar/core/sharded.hh"
 #include "seastar/core/shared_ptr.hh"
 #include "seastar/core/scattered_message.hh"
+#include "schema.hh"
+#include <boost/range/adaptor/transformed.hpp>
+#include <boost/range/adaptor/filtered.hpp>
+
+template<typename ContainerType>
+struct prefetched_struct {
+    const schema_ptr _schema;
+    bool _inited = false;
+    bool _has_more = false;
+    size_t _origin_size = 0;
+    ContainerType _data;
+    prefetched_struct(const schema_ptr schema) : _schema(schema) {}
+    bool has_data() const { return _inited; }
+    bool has_more() const { return _has_more; }
+    void set_has_more(bool v) { _has_more = v; }
+    size_t data_size() const { return _data.size(); }
+    size_t origin_size() const { return _origin_size; }
+    ContainerType& data() { return _data; }
+    const ContainerType& data() const { return _data; }
+};
+
+using prefetched_map_type = prefetched_struct<std::vector<std::pair<std::optional<bytes>, std::optional<bytes>>>>;
+using map_return_type = lw_shared_ptr<prefetched_map_type>;
+using prefetched_zset_type = prefetched_struct<std::vector<std::pair<std::optional<bytes>, std::optional<double>>>>;
+using zset_return_type = lw_shared_ptr<prefetched_zset_type>;
+using prefetched_bytes = prefetched_struct<bytes>;
+using bytes_return_type = lw_shared_ptr<prefetched_bytes>;
+using prefetched_mbytes = prefetched_struct<std::vector<std::pair<bytes, bytes>>>;
+using mbytes_return_type = lw_shared_ptr<prefetched_mbytes>;
+
 namespace redis {
-static const bytes msg_crlf {"\r\n"};
-static const bytes msg_ok {"+OK\r\n"};
-static const bytes msg_pong {"+PONG\r\n"};
-static const bytes msg_err = {"-ERR\r\n"};
-static const bytes msg_zero = {":0\r\n"};
-static const bytes msg_one = {":1\r\n"};
-static const bytes msg_neg_one = {":-1\r\n"};
-static const bytes msg_neg_two = {":-2\r\n"};
-static const bytes msg_null_blik = {"$-1\r\n"};
-static const bytes msg_null_multi_bulk = {"*-1\r\n"};
-static const bytes msg_empty_multi_bulk = {"*0\r\n"};
-static const bytes msg_type_err = {"-WRONGTYPE Operation against a key holding the wrong kind of value\r\n"};
-static const bytes msg_nokey_err = {"-ERR no such key\r\n"};
-static const bytes msg_nocmd_err = {"-ERR no such command\r\n"};
-static const bytes msg_syntax_err = {"-ERR syntax error\r\n"};
-static const bytes msg_same_object_err = {"-ERR source and destination objects are the same\r\n"};
-static const bytes msg_out_of_range_err = {"-ERR index out of range\r\n"};
-static const bytes msg_not_integer_err = {"-ERR ERR hash value is not an integer\r\n" };
-static const bytes msg_not_float_err = {"-ERR ERR hash value is not an float\r\n" };
-static const bytes msg_str_tag = {"+"};
-static const bytes msg_num_tag = {":"};
-static const bytes msg_sigle_tag = {"*"};
-static const bytes msg_batch_tag = {"$"};
-static const bytes msg_not_found = {"+(nil)\r\n"};
-static const bytes msg_nil = {"+(nil)\r\n"};
-static constexpr const int REDIS_OK = 0;
-static constexpr const int REDIS_ERR = 1;
-static constexpr const int REDIS_NONE = -1;
-static constexpr const int REDIS_WRONG_TYPE = -2;
-static const bytes msg_type_string {"+string\r\n"};
-static const bytes msg_type_none {"+none\r\n"};
-static const bytes msg_type_list {"+list\r\n"};
-static const bytes msg_type_hll {"+hyperloglog\r\n"};
-static const bytes msg_type_set {"+set\r\n"};
-static const bytes msg_type_zset {"+zset\r\n"};
-static const bytes msg_type_hash {"+hash\r\n"};
 static const size_t redis_cluser_slots { 16384 };
 class redis_message final {
 private:
-    std::unique_ptr<bytes_ostream> _message;
-    redis_message() noexcept : _message(std::make_unique<bytes_ostream>()) {}
+    seastar::lw_shared_ptr<scattered_message<char>> _message;
+    /*
     redis_message(bytes_view b) noexcept : redis_message() {
         _message->write(b);
     }
+    */
 public:
+    redis_message() = delete;
     redis_message(const redis_message&) = delete;
     redis_message& operator=(const redis_message&) = delete;
     redis_message(redis_message&& o) noexcept : _message(std::move(o._message)) {}
+    redis_message(lw_shared_ptr<scattered_message<char>> m) noexcept : _message(m) {}
     redis_message& operator=(redis_message&& o) noexcept {
         if (this != &o) {
             _message = std::move(o._message);
         }
         return *this;
     }
-   
-    static future<redis_message> make(std::vector<std::tuple<size_t, size_t, bytes, uint16_t>>&& peer_slots) {
-        redis_message m;
-        m.write_sstring(sstring(sprint("*%d\r\n", peer_slots.size())));
-        if (peer_slots.size() > 0) {
-            for (auto& peer_slot : peer_slots) {
-                m.write_sstring(sstring("*3\r\n"));
-                m.write_sstring(sstring(sprint(":%zd\r\n", std::get<0>(peer_slot))));
-                m.write_sstring(sstring(sprint(":%zd\r\n", std::get<1>(peer_slot))));
-                m.write_sstring(sstring(sprint("*2\r\n")));
-                m.write_sstring(sstring(sprint("$%d\r\n", std::get<2>(peer_slot).size())));
-                m.write(bytes_view(std::get<2>(peer_slot)));
-                m.write(bytes_view(msg_crlf));
-                m.write_sstring(sstring(sprint(":%d\r\n", std::get<3>(peer_slot))));
+    static future<redis_message> make_slots(lw_shared_ptr<std::vector<std::tuple<size_t, size_t, bytes, uint16_t>>> peer_slots) {
+        auto m = make_lw_shared<scattered_message<char>> ();
+        m->append(sstring(sprint("*%d\r\n", peer_slots->size())));
+        if (peer_slots->size() > 0) {
+            for (auto& peer_slot : *peer_slots) {
+                m->append(sstring("*3\r\n"));
+                m->append(sstring(sprint(":%zd\r\n", std::get<0>(peer_slot))));
+                m->append(sstring(sprint(":%zd\r\n", std::get<1>(peer_slot))));
+                m->append(sstring(sprint("*2\r\n")));
+                auto& p = std::get<2>(peer_slot);
+                write_bytes(m, p);
+                m->append(sstring(sprint(":%d\r\n", std::get<3>(peer_slot))));
             }
         }
-        return make_ready_future<redis_message>(std::move(m));
+        m->on_delete([ r = std::move(foreign_ptr { peer_slots }) ] {});
+        return make_ready_future<redis_message>(m);
     }
-    bytes_ostream& ostream() { return *_message; }
-    static future<redis_message> make(const bytes& content) {
-        redis_message m;
-        m.write_sstring(sstring(sprint("$%d\r\n", content.size())));
-        m.write(bytes_view(content));
-        m.write(bytes_view(msg_crlf));
-        return make_ready_future<redis_message>(std::move(m));
+    static future<redis_message> make_zset_bytes(lw_shared_ptr<std::vector<std::optional<bytes>>> r);
+    static future<redis_message> make_exception(sstring data) {
+        auto m = make_lw_shared<scattered_message<char>> ();
+        m->append(data);
+        return make_ready_future<redis_message>(m);
     }
-    static future<redis_message> make_exception(bytes&& content) {
-        redis_message m;
-        m.write(bytes_view(content));
-        return make_ready_future<redis_message>(std::move(m));
+    static future<redis_message> make_long(const long content) {
+        auto m = make_lw_shared<scattered_message<char>> ();
+        m->append(sstring(sprint(":%lld\r\n", content)));
+        return make_ready_future<redis_message>(m);
     }
-    static future<redis_message> make(bytes&& content) {
-        redis_message m;
-        m.write_sstring(sstring(sprint("$%d\r\n", content.size())));
-        m.write(bytes_view(content));
-        m.write(bytes_view(msg_crlf));
-        return make_ready_future<redis_message>(std::move(m));
+    static future<redis_message> make_empty_list_bytes() {
+        auto m = make_lw_shared<scattered_message<char>> ();
+        m->append_static("*0\r\n");
+        return make_ready_future<redis_message>(m);
     }
-    static future<redis_message> make(const size_t content) {
-        redis_message m;
-        m.write_sstring(sstring(sprint(":%zu\r\n", content)));
-        return make_ready_future<redis_message>(std::move(m));
+    static future<redis_message> make_list_bytes(map_return_type r, size_t begin, size_t end);
+    static future<redis_message> make_list_bytes(map_return_type r, size_t index);
+    static future<redis_message> make_bytes(bytes_return_type r) {
+        auto m = make_lw_shared<scattered_message<char>> ();
+        assert(r->has_data());
+        write_bytes(m, r->data());
+        m->on_delete([ r = std::move(foreign_ptr { r }) ] {});
+        return make_ready_future<redis_message>(m);
     }
-    static future<redis_message> make(const long content) {
-        redis_message m;
-        m.write_sstring(sstring(sprint(":%lld\r\n", content)));
-        return make_ready_future<redis_message>(std::move(m));
+    static future<redis_message> make_bytes(const bytes& b) {
+        auto m = make_lw_shared<scattered_message<char>> ();
+        m->append(sstring(sprint("$%d\r\n", b.size())));
+        m->append(sstring(reinterpret_cast<const char*>(b.data()), b.size()));
+        m->append_static("\r\n");
+        return make_ready_future<redis_message>(m);
     }
-    static future<redis_message> make(std::vector<bytes>&& content) {
-        redis_message m;
-        m.write_sstring(sstring(sprint("*%zu\r\n", content.size())));
-        for (size_t i = 0; i < content.size(); ++i) {
-            m.write_sstring(sstring(sprint("$%zu\r\n", content[i].size())));
-            m.write(bytes_view(content[i]));
-            m.write(bytes_view(msg_crlf));
-        }
-        return make_ready_future<redis_message>(std::move(m));
-    }
-    static future<redis_message> make(std::vector<std::optional<bytes>>&& content) {
-        redis_message m;
-        m.write_sstring(sstring(sprint("*%zu\r\n", content.size())));
-        for (size_t i = 0; i < content.size(); ++i) {
-            auto& s = *(content[i]);
-            m.write_sstring(sstring(sprint("$%zu\r\n", s.size())));
-            m.write(bytes_view(s));
-            m.write(bytes_view(msg_crlf));
-        }
-        return make_ready_future<redis_message>(std::move(m));
-    }
+    static future<redis_message> make_map_key_bytes(map_return_type r);
+    static future<redis_message> make_map_val_bytes(map_return_type r);
+    static future<redis_message> make_map_bytes(map_return_type r);
+    static future<redis_message> make_set_bytes(map_return_type r, size_t index);
+    static future<redis_message> make_set_bytes(map_return_type r, std::vector<size_t> index);
+    static future<redis_message> make_mbytes(mbytes_return_type r);
     static future<redis_message> one() {
-        redis_message m;
-        m.write(bytes_view(msg_one));
-        return make_ready_future<redis_message>(std::move(m));
+        auto m = make_lw_shared<scattered_message<char>> ();
+        m->append_static(":1\r\n");
+        return make_ready_future<redis_message>(m);
     }
     static future<redis_message> zero() {
-        redis_message m;
-        m.write(bytes_view(msg_zero));
-        return make_ready_future<redis_message>(std::move(m));
+        auto m = make_lw_shared<scattered_message<char>> ();
+        m->append_static(":0\r\n");
+        return make_ready_future<redis_message>(m);
     }
     static future<redis_message> ok() {
-        redis_message m;
-        m.write(bytes_view(msg_ok));
-        return make_ready_future<redis_message>(std::move(m));
+        auto m = make_lw_shared<scattered_message<char>> ();
+        m->append_static("+OK\r\n");
+        return make_ready_future<redis_message>(m);
     }
     static future<redis_message> err() {
-        redis_message m;
-        m.write(bytes_view(msg_zero));
-        return make_ready_future<redis_message>(std::move(m));
+        auto m = make_lw_shared<scattered_message<char>> ();
+        m->append_static(":0\r\n");
+        return make_ready_future<redis_message>(m);
     }
     static future<redis_message> null() {
-        redis_message m;
-        m.write(bytes_view(msg_null_blik));
-        return make_ready_future<redis_message>(std::move(m));
+        auto m = make_lw_shared<scattered_message<char>> ();
+        m->append_static("$-1\r\n");
+        return make_ready_future<redis_message>(m);
     }
+    inline lw_shared_ptr<scattered_message<char>> message() { return _message; }
 private:
-    inline void write(const bytes_view content) {
-        _message->write(content);
-    }
-    inline void write_sstring(sstring&& content) {
-        _message->write(bytes_view(reinterpret_cast<const int8_t*>(content.data()), content.size()));
+    static void write_bytes(lw_shared_ptr<scattered_message<char>> m, bytes& b) {
+        m->append(sstring(sprint("$%d\r\n", b.size())));
+        m->append_static(reinterpret_cast<const char*>(b.data()), b.size());
+        m->append_static("\r\n");
     }
 };
 }
